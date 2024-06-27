15cce4a489cdbb2c122de0c6d33156c1249cd421
Lcarrizo221 <lucascarrizo566@gmail.com>
2024-06-27 20:51:55 -0300

    subida de proyecto a el repo

---
+++ app.js
@@ -0,0 +1,21 @@
+const PORT = 3030;
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
